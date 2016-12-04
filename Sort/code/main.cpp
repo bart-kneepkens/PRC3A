a60e4015234f2f7f7438751720dd9ca0d9f6b0c1
Martin Sebastiaan Donkersloot <martin.donkersloot@outlook.com>
2016-12-04 02:09:23 +0100

    Bubble sort seems to work. Hip hip, hurray.

--- Sort/code/main.cpp
+++ Sort/code/main.cpp
@@ -1,94 +1,141 @@
 #include <iostream>
-//#include <cstddef> // needed for definition of NULL
-
+#include <fstream>
 #include "FileStructure.h"
+#include<stdio.h>
+#include<stdlib.h>
 
-void split(File * source, File ** front, File ** back){
-    File* fast;
-    File* slow;
-    
-    if(source == NULL || source->getNext() == NULL){
-        *front = source;
-        *back = NULL;
-    } else {
-        slow = source;
-        fast = source->getNext();
-        
-        while (fast != NULL){
-            fast = fast->getNext();
-            if(fast != NULL){
-                slow = slow->getNext();
-                fast = fast->getNext();
+void printFileNames(File *file, std::ofstream &out, std::string path = "") {
+    while (file != NULL) {
+        out << (path + file->getName()) << "\n";
+        file = file->getNext();
+    }
+}
+
+void printDirNames(Dir *dir, std::ofstream &out, std::string path = "") {
+    while (dir != NULL) {
+        const std::string childPath = path + dir->getName() + "/";
+        out << childPath << "\n";
+        printDirNames(dir->getFirstSubdir(), out, childPath);
+        printFileNames(dir->getFirstFile(), out, childPath);
+        dir = dir->getNext();
+    }
+}
+
+void bubbleSortFiles(File **head) {
+
+    unsigned int count = 0;
+    File *start = *head;
+
+    while (start != NULL) { //grab count
+        count++;
+        start = start->getNext();
+    }
+
+    for (unsigned int i = 0; i < count; ++i) { //for every element in the list
+
+        File *curr = *head;
+        File *trail = *head;
+
+        while (curr->getNext() != NULL) { //for the rest of the elements in the list
+            if (curr->getName() > curr->getNext()->getName()) { //compare curr and curr->next
+
+                File *temp = curr->getNext(); //swap pointers for curr and curr->next
+                curr->setNext(temp->getNext());
+                temp->setNext(curr);
+
+                //now we need to setup pointers for trail and possibly head
+                if (curr == *head) { //this is the case of the first element swapping to preserve the head pointer
+                    *head = trail = temp;
+                } else { //setup trail correctly
+                    trail->setNext(temp);
+                }
+                curr = temp; //update curr to be temp since the positions changed
             }
+            //advance pointers
+            trail = curr;
+            curr = curr->getNext();
         }
-        
-        *front = source;
-        *back = slow->getNext();
-        slow->setNext(NULL);
     }
 }
 
-File* sorted(File* a, File* b){
-    File* result = NULL;
-    
-    if(a == NULL)
-        return(b);
-    else if(b == NULL)
-        return(a);
-    
-    if(a->getName().compare(b->getName())){
-        result = a;
-        result->setNext(sorted(a->getNext(), b));
-    } else {
-        result = b;
-        result->setNext(sorted(a, b->getNext()));
+void bubbleSortDirs(Dir **head) {
+
+    unsigned int count = 0;
+    Dir *start = *head;
+
+    while (start != NULL) { //grab count
+        Dir *firstSubdir = start->getFirstSubdir();
+        Dir **firstSubDirPtr = &firstSubdir;
+        bubbleSortDirs(firstSubDirPtr);
+        start->setFirstSubdir(*firstSubDirPtr);
+
+        File *firstFile = start->getFirstFile();
+        File **firstFilePtr = &firstFile;
+        bubbleSortFiles(firstFilePtr);
+        start->setFirstFile(*firstFilePtr);
+
+        count++;
+        start = start->getNext();
     }
-    return result;
+
+    for (unsigned int i = 0; i < count; ++i) { //for every element in the list
+
+        Dir *curr = *head;
+        Dir *trail = *head;
+
+        while (curr->getNext() != NULL) { //for the rest of the elements in the list
+            if (curr->getName() > curr->getNext()->getName()) { //compare curr and curr->next
+
+                Dir *temp = curr->getNext(); //swap pointers for curr and curr->next
+                curr->setNext(temp->getNext());
+                temp->setNext(curr);
+
+                //now we need to setup pointers for trail and possibly head
+                if (curr == *head) { //this is the case of the first element swapping to preserve the head pointer
+                    *head = trail = temp;
+                } else { //setup trail correctly
+                    trail->setNext(temp);
+                }
+                curr = temp; //update curr to be temp since the positions changed
+            }
+            //advance pointers
+            trail = curr;
+            curr = curr->getNext();
+        }
+    }
 }
 
 
-void mergeSort(File** headref){
-    File * head = *headref;
-    
-    File * a;
-    File * b;
-    
-    split(head, &a, &b);
-    
-    mergeSort(&a);
-    mergeSort(&b);
-    
-    *headref = sorted(a,b);
-}
-
-
-int main()
-{
+int main() {
     FileStructure f;
-    Dir head;
-    
-    f.loadFile("data/gibberish.bin", head);
+    Dir headVal;
 
-    // next line is only to show what kind of data we're working with
-    // remove this line to increase performance!
-    
-    
-    // sort all data
-    // todo: your code here!
-    
-    
-    // sort dirs
-    
-    
-    // sort files
-    File* headFile = head.getFirstFile();
-    
-    mergeSort(&headFile);
+    std::cout << "Loading 'data/gibberish.bin'... " << std::flush;
+    f.loadFile("data/gibberish.bin", headVal);
+    std::cout << "Done!" << std::endl;
 
-    //std::cout << head.toString(false);
+    Dir *head = &headVal;
+    Dir **headPtr = &head;
 
-    // save sorted data into a new file called sorted.bin
-    f.saveFile(head, "sorted.bin");
+    std::cout << "Saving to 'unsorted.bin'... " << std::flush;
+    f.saveFile(**headPtr, "unsorted.bin");
+    std::cout << "Done!" << std::endl;
+
+    std::ofstream unsortedfile("unsorted.txt");
+    printDirNames(*headPtr, unsortedfile);
+    unsortedfile.close();
+
+    std::cout << "Applying bubble sort... " << std::flush;
+    bubbleSortDirs(headPtr);
+    std::cout << "Done!" << std::endl;
+
+    std::ofstream sortedfile("sorted.txt");
+    printDirNames(*headPtr, sortedfile);
+    sortedfile.close();
+
+    std::cout << "Saving to 'sorted.bin'... " << std::flush;
+    f.saveFile(**headPtr, "sorted.bin");
+    std::cout << "Done!" << std::endl;
 
     return 0;
 }
